1e37918d10bcb6f65d2eec5d1b7a0e978c240453
maksimkaSH <shteinmaks@gmail.com>
2025-03-09 15:37:46 +0300

    task 1

---
+++ hw-1/src/hw_1.1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+
+
+def print_lines_with_number(input_stream):
+    for i, line in enumerate(input_stream, start=1):
+        print(f"{i}\t{line}", end='')
+
+
+def main():
+    if len(sys.argv) == 1:
+        print_lines_with_number(sys.stdin)
+    elif len(sys.argv) == 2:
+        file_path = sys.argv[1]
+        try:
+            with open(file_path, 'r') as file:
+                print_lines_with_number(file)
+        except FileNotFoundError:
+            print(f"task1: {file_path}: No such file or directory")
+
+
+if __name__ == "__main__":
+    main()
